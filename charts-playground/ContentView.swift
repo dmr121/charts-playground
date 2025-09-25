88757a3bc02d1d506e5fa7325110edfa4f6767cb
dmr121 <dmrozmajzl@gmail.com>
2025-09-24 22:54:19 -0400

    Initial Commit

---
+++ charts-playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  charts-playground
+//
+//  Created by David Rozmajzl on 9/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ charts-playground/charts_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  charts_playgroundApp.swift
+//  charts-playground
+//
+//  Created by David Rozmajzl on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct charts_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
